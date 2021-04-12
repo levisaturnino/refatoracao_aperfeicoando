2ba4253f53cd4c3af085ae7856d2a8d9f74efdd9
Levi Saturnino <levisaturnino@gmail.com>
2021-04-12 11:53:02 -0300

    substituir variavel temporaria por consulta

--- src/main/kotlin/br/com/levisaturnino/refatoring/controllers/PlayController.kt
+++ src/main/kotlin/br/com/levisaturnino/refatoring/controllers/PlayController.kt
@@ -13,7 +13,7 @@ class PlayController {
         var result = "Statement for ${invoice.customer}\n"
 
         for ((index, perf)  in invoice.performances?.withIndex()!!) {
-            val play = plays[index]
+            val play = playFor(index,plays)
 
             var thisAmount = amount(perf,play)
 
@@ -41,27 +41,31 @@ class PlayController {
         return currencyFormatter.format(amount)
     }
 
+    fun playFor(index: Int,plays:List<IPlay>):IPlay{
+        return plays[index]
+    }
+
     fun amount(perf:Performance, play: IPlay):Int{
 
-        var thisAmount = 0
+        var result  = 0
+
         when (play.type()) {
             "tragedy" -> {
-                thisAmount = 40000
+                result = 40000
                 if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30)
+                    result += 1000 * (perf.audience - 30)
                 }
             }
             "comedy" -> {
-                thisAmount = 30000
+                result = 30000
                 if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20)
+                    result += 10000 + 500 * (perf.audience - 20)
                 }
-                thisAmount += 300 * perf.audience
+                result += 300 * perf.audience
             }
             else ->
                 throw  Error ("unknown type: ${play.type()}")
         }
-
-        return thisAmount
+        return result
     }
 }
